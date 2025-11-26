8c2457eea3808645c68ffb7233778498b5cac72b
ptakguillaume-cpu <ptakguillaume@gmail.com>
2025-11-26 10:16:43 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'kty-app-v3'; // Augmentez le chiffre à chaque mise à jour pour forcer le refresh
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './data.json',
+  './manifest.json',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
